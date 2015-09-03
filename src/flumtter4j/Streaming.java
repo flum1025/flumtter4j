8ef2dd0d5bf3fd87f5fd7c891934321b7e7a2006
ふらんちゃん <flum.1025@gmail.com>
2015-09-03 18:29:17 +0900

    必殺!!!ｵｰﾊﾞｰﾗｲﾄﾞ!!!!!!!!!!!!!

--- src/flumtter4j/Streaming.java
+++ src/flumtter4j/Streaming.java
@@ -11,27 +11,33 @@ import twitter4j.UserStreamAdapter;
 public class Streaming extends UserStreamAdapter {
 	public static ArrayList<Status> statues = new ArrayList<Status>();
 	
+	@Override
 	public void onStatus(Status status){
 		statues.add(status);
 		Tcallback.tcallback.on_TweetObject(status);
 	}
 	
+	@Override
 	public void onDeletionNotice(StatusDeletionNotice object){
 		Dcallback.dcallback.on_DeleteObject(object);
 	}
 	
+	@Override
 	public void onFavorite(User source, User target, Status object){
 		statues.add(object);
 		Favcallback.favcallback.on_FavoriteObject(source, target, object);
 	}
 	
+	@Override
 	public void onUnfavorite(User source, User target, Status object){
 	}
 	
+	@Override
 	public void onDirectMessage(DirectMessage object){
 		DMcallback.dmcallback.on_DirectMessageObject(object);
 	}
 	
+	@Override
 	public void onFollow(User source, User followedUser){
 	}
 }
